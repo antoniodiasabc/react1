66e3c85c09f141dbecab292b946cd504213feb47
Antonio <adias@atech.com.br>
2022-05-07 18:10:42 -0300

    first commit

---
+++ src/Welcome.js
@@ -0,0 +1,6 @@
+function Welcome(props) {
+    return (<h1>Bom dia, {props.name} Hoje é {props.data}</h1>);
+  }
+  
+ 
+  export default Welcome;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Welcome from './Welcome';
+
+const name = "Antony";
+const data = new Date().toISOString();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+       <Welcome  name={name} data={data} />
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
